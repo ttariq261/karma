1e7ae3a0b500a51dfb914edf44f1cfaf65c3dfc7
tariq <ttariq261@gmail.com>
2014-03-07 11:20:20 +0000

    sdgsdgs

---
+++ www/js/routes.js
@@ -0,0 +1,30 @@
+"use strict";
+
+angular.module('todo.routes', ['ngRoute'])
+
+   // configure views; the authRequired parameter is used for specifying pages
+   // which should only be available while logged in
+   .config(['$routeProvider', function($routeProvider) {
+      $routeProvider.when('/home', {
+         templateUrl: 'partials/home.html',
+         controller: 'HomeCtrl'
+      });
+
+      $routeProvider.when('/play', {
+         templateUrl: 'views/play.html',
+         controller: 'PlayCtrl'
+      });
+
+      $routeProvider.when('/account', {
+         authRequired: true, // must authenticate before viewing this page
+         templateUrl: 'partials/account.html',
+         controller: 'AccountCtrl'
+      });
+
+      $routeProvider.when('/login', {
+         templateUrl: 'partials/login.html',
+         controller: 'LoginCtrl'
+      });
+
+      $routeProvider.otherwise({redirectTo: '/home'});
+   }]);
